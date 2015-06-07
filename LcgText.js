a36a536bc51f6fb5b8c7f5e2e0f0f8e1fcfb6d10
92Garfield <profi.zocker@yahoo.com>
2015-06-07 21:51:54 +0200

    Added attributes for text.

--- LcgText.js
+++ LcgText.js
@@ -3,20 +3,24 @@
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 function LcgText() {
-  LcgDisplayObjectContainer.call(this);
-  this.text = '';
-  /**
-		* Overloaded draw function, draws this.text on the screen
-		*
-		* @param 	{g} 		The canvas context that will draw the text
-		* @param 	{offsetX} 	The X coordinate of the drawn text
-		* @param 	{offsetY} 	The Y coordinate of the drawn text
-		* @return 	{}
-		*/
-  this.draw = function (g, offsetX, offsetY) {
-    var myX = offsetX + this.x;
-    var myY = offsetY + this.y;
-    g.font = '20px Georgia';
-    g.fillText(this.text, myX, myY);
-  }
+    LcgDisplayObjectContainer.call(this);
+    this.text = '';
+    this.font = 'Georgia';
+    this.size = 20;
+    this.color = "#000000";
+    /**
+     * Overloaded draw function, draws this.text on the screen
+     *
+     * @param 	{g} 		The canvas context that will draw the text
+     * @param 	{offsetX} 	The X coordinate of the drawn text
+     * @param 	{offsetY} 	The Y coordinate of the drawn text
+     * @return 	{}
+     */
+    this.draw = function (g, offsetX, offsetY) {
+        var myX = offsetX + this.x;
+        var myY = offsetY + this.y;
+        g.font = this.size + "px " + this.font;
+        g.fillStyle = this.color;
+        g.fillText(this.text, myX, myY);
+    };
 }

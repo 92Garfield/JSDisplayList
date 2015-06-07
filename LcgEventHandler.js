58453c19e6fd37c82c7efd6eab072d7ec8b7e6e4
Youssef/Yousif Rehab <loufis@aucegypt.edu>
2015-06-07 23:09:09 +0300

    Modified parameters names to be more comprehensive

--- LcgEventHandler.js
+++ LcgEventHandler.js
@@ -4,19 +4,19 @@ function LcgEventHandler() {
 
 	this.events = {};
 
-	this.addEventListener = function(event, callback) {
+	this.addEventListener = function(eventStringLiteral, callback) {
 
-		this.events.hasOwnProperty(event) || (this.events[event] = []);
-		this.events[event].push(callback);
+		this.events.hasOwnProperty(eventStringLiteral) || (this.events[eventStringLiteral] = []);
+		this.events[eventStringLiteral].push(callback);
 	};
 
-	this.dispatchEvent = function(event) {
+	this.dispatchEvent = function(eventObject) {
 
-		var callbacks = this.events[event[0]];
+		var callbacks = this.events[eventObject[0]];
 		if (callbacks.length == 0) return false;
 
 		for(var i = 0, l = callbacks.length; i < l; i++) {
-			callbacks[i](event[1]);
+			callbacks[i](eventObject[1]);
 		};
 
 		return true;

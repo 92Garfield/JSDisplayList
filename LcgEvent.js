ff063be6abc1acb14371dc2a857d699d04fd4761
Youssef/Yousif Rehab <loufis@aucegypt.edu>
2015-06-08 20:46:52 +0300

    Added comments to LcgEvent.js

--- LcgEvent.js
+++ LcgEvent.js
@@ -1,16 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////////////////
+// LcgEvent class
+// A class for creating events
+// For event handling and attaching events, see LcgEventHandler
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// List of available events
+////////////////////////////////////////////////////////////////////////////////////////////
 LcgEvent.CLICK = "LCG_EVENT_CLICK";
-
 LcgEvent.LOAD = "LCG_EVENT_LOAD";
+////////////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * @private
- */
+
+
+
+
+// List of propertie names to be copied from the event passed to the constructor
 LcgEvent.copyProperties = ["timeStamp", "isTrusted", "isChar", "pageX", "pageY",
     "altKey", "button", "clientX", "clientY", "ctrlKey",
     "metaKey", "movementX", "movementY", "screenX", "screenY",
     "shiftKey", "which", "mozInputSource"
 ];
 
+
+
+/**
+* @constructor
+* Searches for a DisplayObject in the displayList
+* @param    {string}        The name of the event type, must be one of the predefined events, can be obtained from LcgEvent."PREDEFINED_EVENT_NAME"
+* @param    {jsEvent}       The original jsEvent that will be used to construct LcgEvent
+*/
 function LcgEvent(type, event) {
     this.currentTarget = null;
     this.type = type;

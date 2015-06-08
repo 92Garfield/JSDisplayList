6f1f76cd9cd4b3b5ee00ad31a471044ce9226d2f
92Garfield <profi.zocker@yahoo.com>
2015-06-08 14:38:03 +0200

    Updated with LcgEvent.

--- LcgEventHandler.js
+++ LcgEventHandler.js
@@ -5,19 +5,17 @@ function LcgEventHandler() {
     this.events = {};
 
     this.addEventListener = function (eventStringLiteral, callback) {
-
         this.events.hasOwnProperty(eventStringLiteral) || (this.events[eventStringLiteral] = []);
         this.events[eventStringLiteral].push(callback);
     };
 
     this.dispatchEvent = function (eventObject) {
-
-        var callbacks = this.events[eventObject[0]];
+        var callbacks = this.events[eventObject.type];
         if (!callbacks || callbacks.length == 0)
             return false;
 
         for (var i = 0; i < callbacks.length; i++) {
-            callbacks[i](eventObject[1]);
+            callbacks[i](eventObject);
         }
         return true;
     };

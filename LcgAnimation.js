045e23ff08abb3f4c8d07ef32840edd600ab4916
92Garfield <profi.zocker@yahoo.com>
2015-06-22 18:40:07 +0200

    Added commendt to LcgAnimation.

--- LcgAnimation.js
+++ LcgAnimation.js
@@ -1,3 +1,16 @@
+/**
+ * @constructor
+ * @param {String} location The path to the folder with the frames. (ending with "/")
+ * @param {Number} frameTime The time between frames.
+ * @param {Number} endTime The time after the last Animation frame.
+ * @param {Number} startFrame The frame to start the Animation,
+ * @param {Number} frameCount Amount of frames in the folder at location.
+ * @param {Number} minChanges Minimum amount of frame changes before end.
+ * @param {Number} endFrame The last frame of the Animation.
+ * @param {String} ext File extionsion. (without ".")
+ * @returns {LcgAnimation}
+ */
+
 function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minChanges, endFrame, ext) {
     LcgDisplayObjectContainer.call(this);
     
@@ -22,6 +35,11 @@ function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minC
         this.addChildAt(curFrame, 0);
     }
     
+    /**
+     * @public
+     * Makes the next frame to the top-most in the displaylist
+     * @return 	{void}
+     */
     this.updateFrames = function() {
         var prevFrame = this.getChildAt(this.getNumChildren() - 1);
         this.removeChild(prevFrame);

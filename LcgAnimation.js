6f661e8eafc765e21a18c5173042484e44adf881
Youssef/Yousif Rehab <loufis@aucegypt.edu>
2015-07-25 18:08:04 +0300

    Modified and Fixed some bugs in LcgAnimation.js

--- LcgAnimation.js
+++ LcgAnimation.js
@@ -2,16 +2,16 @@
  * @constructor
  * @param {String} location The path to the folder with the frames. (ending with "/")
  * @param {Number} frameTime The time between frames.
- * @param {Number} endTime The time after the last Animation frame.
+ * @param {Number} endTime The time after the last Animation frame, set to -1 if the last frame will persist indefinitely.
  * @param {Number} startFrame The frame to start the Animation,
  * @param {Number} frameCount Amount of frames in the folder at location.
- * @param {Number} minChanges Minimum amount of frame changes before end.
+ * @param {Number} minCycles Minimum amount of full cycles before end.
  * @param {Number} endFrame The last frame of the Animation.
  * @param {String} ext File extionsion. (without ".")
  * @returns {LcgAnimation}
  */
 
-function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minChanges, endFrame, ext) {
+function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minCycles, endFrame, ext) {
     LcgDisplayObjectContainer.call(this);
     
     this.path = location;
@@ -20,9 +20,9 @@ function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minC
     this.startFrame = startFrame;
     this.frameCount = frameCount;
     this.currentFrame = startFrame;
-    this.minChanges = minChanges;
+    this.minCycles = minCycles;
     this.endFrame = endFrame;
-    this.changes = 0;
+    this.cycles = 0;
     this.ext = ext;
     
     var frames = [];
@@ -41,26 +41,28 @@ function LcgAnimation(location, frameTime, endTime, startFrame, frameCount, minC
      * @return 	{void}
      */
     this.updateFrames = function() {
+        this.currentFrame = (this.currentFrame + 1) % this.frameCount;
+        if (this.currentFrame == this.startFrame)
+            this.cycles++;
         var prevFrame = this.getChildAt(this.getNumChildren() - 1);
         this.removeChild(prevFrame);
         this.addChildAt(prevFrame, 0);
     };
     
     function changeFrame() {
-        if (_this.changes >= minChanges && _this.currentFrame == endFrame) {
-            var aniEvent = new LcgEvent(LcgEvent.ANIMATION_FINISHED, {});
-            aniEvent.currentTarget = _this;
-            setTimeout(function() {
-                _this.dispatchEvent(aniEvent);
-            }, _this.endTime);
-            
-            return;
+        if (_this.cycles >= minCycles && _this.currentFrame == endFrame) {
+            if (_this.endTime != -1) {
+                var aniEvent = new LcgEvent(LcgEvent.ANIMATION_FINISHED, null);
+                aniEvent.currentTarget = _this;
+                setTimeout(function() {
+                    _this.dispatchEvent(aniEvent);
+                }, _this.endTime);
+            }
         }
-        
-        _this.currentFrame = (_this.currentFrame + 1) % _this.frameCount;
-        _this.updateFrames();
-        setTimeout(changeFrame, frameTime);
-        _this.changes++;
+        else {
+            _this.updateFrames();
+            setTimeout(changeFrame, frameTime);    
+        }
     }
     
     setTimeout(changeFrame, frameTime);
